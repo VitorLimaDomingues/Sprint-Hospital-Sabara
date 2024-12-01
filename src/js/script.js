024a576065cfaa53c67be1af1329b92ae8ad07e2
Giovanni <giovannirprovazi@gmail.com>
2024-12-01 14:36:08 -0300

    Criando página de contato

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+function telefone() {
+    alert("Nosso telefone fixo é 11 98783-0521");
+}
+
+function email() {
+    alert("O nosso email é nutrisabara@gmail.com");
+}
+
+function wpp() {
+    alert("Nosso whatsapp é 11 93454-6570");
+}
+
+function insta() {
+    alert("Nosso instagram é @nutri_sabara");
+}
+
+function twitter() {
+    alert("Nosso twitter é @nutrisabara_ofc");
+}
